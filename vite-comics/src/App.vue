3d7edd6d57e0158e80fb38d2f42e9fae28a1e1a1
robertoraciti <rob.raciti97@gmail.com>
2023-09-10 18:52:29 +0200

    first commit

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template></template>
+
+<style></style>
---
+++ vite-comics/src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style></style>
