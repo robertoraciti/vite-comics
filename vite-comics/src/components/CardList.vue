0da711143bb9ad3ba9897aa07d0c8a37a1f69844
robertoraciti <rob.raciti97@gmail.com>
2023-09-11 16:17:26 +0200

    complete ex without props

---
+++ vite-comics/src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    thumb: String,
+    series: String,
+  },
+};
+</script>
+<template>
+  <!-- <div class="card">
+    <img :src="thumb" alt="" />
+    <h2 class="title">{{ series }}</h2>
+  </div> -->
+</template>
+<style lang="scss">
+// .card {
+//   width: calc(100% / 6);
+// }
+</style>
